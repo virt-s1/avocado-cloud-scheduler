1a9fbe1f83b4e7bbbc87474cf5410daf6926d53d
Charles Shih <schrht@gmail.com>
2021-11-29 14:46:06 +0800

    Implement class ContainerAssistant.

--- scheduler.py
+++ scheduler.py
@@ -33,98 +33,123 @@ UTILS_PATH = './utils'
 class ContainerAssistant():
     """Deal with container resources."""
 
-    def __init__(self, config={}):
-        container_path = config.get('container_path', '/tmp')
-        container_pool = config.get('container_pool', [])
-        container_image = config.get('container_image')
+    def __init__(self, config_file):
+        # Load and parse user config
+        with open(config_file, 'r') as f:
+            config = toml.load(f)
+            LOG.debug(f'{ARGS.config}: {config}')
 
-        for container_name in container_pool:
-            # check the status
-            _path = os.path.join(container_path, container_name)
-            if os.path.isdir(_path):
-                pass
-            else:
-                LOG.error(f'Invalid container path "{_path}".')
-                exit(1)
+        container_image = config.get('containers', {}).get('container_image')
+        container_path = config.get('containers', {}).get('container_path')
+        container_pool = config.get('containers', {}).get('container_pool')
 
+        # Verify container image
+        cmd = f'podman inspect {container_image} &>/dev/null'
+        res = subprocess.run(cmd, shell=True)
+        if res.returncode == 0:
+            LOG.debug(f'Container image "{container_image}" is valid.')
+        else:
+            LOG.error(f'Container image "{container_image}" is invalid.')
+            exit(1)
+
+        # Create container path if needed
+        os.makedirs(container_path, exist_ok=True)
+        LOG.debug(f'Get user config "container_path": {container_path}')
+
+        # Verify container pool
+        if not isinstance(container_pool, list):
+            LOG.error('The container_pool (should be list) is invalid.')
+            exit(1)
+        else:
+            LOG.debug(f'Get user config "container_pool": {container_pool}')
+
+        self.container_image = container_image
         self.container_path = container_path
         self.container_pool = container_pool
-        self.container_image = container_image
 
-    def list_all_containers(self):
-        return self.container_pool
+    def get_container_status(self):
+        """Get the status of containers in the pool.
 
-    def list_available_containers(self):
-        available_containers = []
-        for container_name in self.container_pool:
-            cmd = f'podman ps -a --format "{{{{.Names}}}}" \
-                | grep -q -x {container_name}'
+        Input:
+            N/A
+        Output:
+            - dict of the container status
+        """
+        status = {}
+        for name in self.container_pool:
+            cmd = f'podman inspect {name} &>/dev/null'
             res = subprocess.run(cmd, shell=True)
-            LOG.debug(res)
-            if res.returncode > 0:
-                available_containers.append(container_name)
+            if res.returncode == 0:
+                status[name] = 'unavailable'
+            else:
+                status[name] = 'available'
 
-        LOG.debug(f'available_containers: {available_containers}')
-        return available_containers
+        LOG.debug(f'Container Status: {status}')
 
-    def get_available_container(self):
-        available_containers = self.list_available_containers()
+        return status
 
-        if len(available_containers) > 0:
-            container = available_containers[0]
-            LOG.debug(f'Got container "{container}"')
-            return container
-        else:
-            LOG.debug('No available container left.')
-            return None
+    def pick_container(self):
+        """Pick an container for the test.
 
-    def trigger_container_run(self, container_name):
-        # Prepare environment
-        container_data_path = os.path.join(
-            self.container_path, container_name, 'data')
-        container_result_path = os.path.join(
-            self.container_path, container_name, 'job-results')
+        Input:
+            N/A
+        Output:
+            - container (string) or '' if no available ones
+        """
+        status = self.get_container_status()
+        for name in status.keys():
+            if status[name] == 'available':
+                LOG.info(f'Picked container "{name}" '
+                         f'from "{self.container_pool}".')
+                return name
 
-        os.makedirs(container_data_path, exist_ok=True)
-        os.makedirs(container_result_path, exist_ok=True)
+    # def trigger_container_run(self, container_name):
+    #     # Prepare environment
+    #     container_data_path = os.path.join(
+    #         self.container_path, container_name, 'data')
+    #     container_result_path = os.path.join(
+    #         self.container_path, container_name, 'job-results')
 
-        #os.unlink(os.path.join(container_result_path, 'latest'))
+    #     os.makedirs(container_data_path, exist_ok=True)
+    #     os.makedirs(container_result_path, exist_ok=True)
 
-        subprocess.run(f'chcon -R -u system_u -t svirt_sandbox_file_t \
-            {self.container_path}', shell=True)
+    #     #os.unlink(os.path.join(container_result_path, 'latest'))
 
-        # Execute the test
-        LOG.info(f'Run test in container "{container_name}"...')
-        cmd = f'podman run --name {container_name} --rm -it \
-            -v {container_data_path}:/data:rw \
-            -v {container_result_path}:/root/avocado/job-results:rw \
-            {self.container_image} /bin/bash ./container/bin/test_alibaba.sh'
+    #     subprocess.run(f'chcon -R -u system_u -t svirt_sandbox_file_t \
+    #         {self.container_path}', shell=True)
 
-        LOG.info(f'Run container with command: \n{cmd}')
-        test_result = subprocess.run(cmd, shell=True)
+    #     # Execute the test
+    #     LOG.info(f'Run test in container "{container_name}"...')
+    #     cmd = f'podman run --name {container_name} --rm -it \
+    #         -v {container_data_path}:/data:rw \
+    #         -v {container_result_path}:/root/avocado/job-results:rw \
+    #         {self.container_image} /bin/bash ./container/bin/test_alibaba.sh'
 
-        # Postprocess the logs
-        import time
-        time.sleep(10)
-        LOG.info('Postprocessing logs...')
-        os.makedirs(os.path.join(container_result_path,
-                    'latest', 'testinfo'), exist_ok=True)
-        shutil.copy(os.path.join(container_data_path, 'alibaba_common.yaml'),
-                    os.path.join(container_result_path, 'latest', 'testinfo',
-                                 'alibaba_common.yaml'))
-        shutil.copy(os.path.join(container_data_path, 'alibaba_testcases.yaml'),
-                    os.path.join(container_result_path, 'latest', 'testinfo',
-                    'alibaba_testcases.yaml'))
-        shutil.copy(os.path.join(container_data_path, 'alibaba_flavors.yaml'),
-                    os.path.join(container_result_path, 'latest', 'testinfo',
-                    'alibaba_flavors.yaml'))
+    #     LOG.info(f'Run container with command: \n{cmd}')
+    #     test_result = subprocess.run(cmd, shell=True)
 
-        if test_result.returncode == 0:
-            LOG.info(f'Test succeed in container "{container_name}".')
-            return 0
-        else:
-            LOG.warning(f'Test failed in container "{container_name}".')
-            return 1
+    #     # Postprocess the logs
+    #     import time
+    #     time.sleep(10)
+    #     LOG.info('Postprocessing logs...')
+    #     os.makedirs(os.path.join(container_result_path,
+    #                 'latest', 'testinfo'), exist_ok=True)
+    #     shutil.copy(os.path.join(container_data_path, 'alibaba_common.yaml'),
+    #                 os.path.join(container_result_path, 'latest', 'testinfo',
+    #                              'alibaba_common.yaml'))
+    #     shutil.copy(os.path.join(container_data_path, 'alibaba_testcases.yaml'),
+    #                 os.path.join(container_result_path, 'latest', 'testinfo',
+    #                 'alibaba_testcases.yaml'))
+    #     shutil.copy(os.path.join(container_data_path, 'alibaba_flavors.yaml'),
+    #                 os.path.join(container_result_path, 'latest', 'testinfo',
+    #                 'alibaba_flavors.yaml'))
+
+    #     if test_result.returncode == 0:
+    #         LOG.info(f'Test succeed in container "{container_name}".')
+    #         return 0
+    #     else:
+    #         LOG.warning(f'Test failed in container "{container_name}".')
+    #         return 1
 
 
 class CloudAssistant():
@@ -334,6 +359,7 @@ class TestScheduler():
     """Schedule the containerized avocado-cloud testing."""
     pass
 
+
 class TestWorker():
     """Execute the containerized avocado-cloud testing."""
 
@@ -348,7 +374,7 @@ class TestWorker():
         return self.cloud_assistant.get_available_azone(flavor)
 
     def _get_container(self):
-        return 
+        return
 
     def _provision_test(self, container_name, flavor, azone):
         self.config_assistant.provision_data(
@@ -377,7 +403,7 @@ class TestWorker():
         azone = self.cloud_assistant.pick_azone(flavor)
 
         # Get container
-        container = self.container_assistant.get_available_container()
+        container = self.container_assistant.pick_container()
 
         exit(0)
         # Provision data
@@ -392,10 +418,6 @@ class TestWorker():
 
 
 if __name__ == '__main__':
-    # scheduler = TestScheduler(ARGS)
-    # scheduler.signle_test(flavor='ecs.i2.xlarge')
-
-
     # Load and parse user config
     with open(ARGS.config, 'r') as f:
         config = toml.load(f)
@@ -427,7 +449,7 @@ if __name__ == '__main__':
     scheduler = TestScheduler()
     worker = TestWorker()
 
-    worker.start()
+    worker.start('ecs.i2.xlarge')
 
 
 exit(0)
